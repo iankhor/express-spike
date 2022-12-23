52b25317342aa1213ba8f8fd43e59d3d351621d5
iankhor <iankhorbc.thecoder@gmail.com>
2022-12-23 18:02:54 +1100

    refactor

--- src/server/db/models/index.js
+++ src/server/db/models/index.js
@@ -1,17 +1,9 @@
-import path from 'path'
 import Sequelize, {DataTypes} from 'sequelize'
 import process from 'process'
 
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 const Community = sequelize.define('Community', {
   firstName: DataTypes.STRING,
